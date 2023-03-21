edac42d80935292e44033188df56239970e6333d
Papachips <38638070+Papachips@users.noreply.github.com>
2023-03-21 19:36:40 -0400

    rewrite and comments
    refactored code to remove obsolete lines, fix bugs, streamline, and comment so it's more clear what is going on

--- kregify.py
+++ kregify.py
@@ -15,33 +15,47 @@ def formatAndUpload():
     artistInfo = dict()
 
     for file in fileList:
+        #splits the file name and extension. py7zr handles .tar and .7z, so we check for those first as most often that's what we have.
         if(os.path.splitext(file)[1] == '.tar'or os.path.splitext(file)[1] == '.7z'):
             filePath = directory + '/' + file
             archive = py7zr.SevenZipFile(filePath, mode='r')
             archive.extractall(path=directory)
             archive.close()
+            os.remove(filePath)
+
+        #py7zr does not handle .zip files, so we use zipfile for that.
         if(os.path.splitext(file)[1] == '.zip'):
             with zipfile.ZipFile(directory + '/' + file, 'r') as zip:
                 zip.extractall(directory)
+                os.remove(filePath)
 
-    for path, subdirs, files in os.walk(directory):
-        for name in files:
-            file = os.path.join(path, name)
-            os.chdir(path)
+    #iterate over all the decompressed directories folders
+    for path in glob.glob(directory + '/*/'):
+        for folderPath in os.walk(path):
+            #change directory so when filebot runs, it runs against the correct album directory
+            os.chdir(folderPath[0])
 
-            try:
-                os.chmod(file, stat.S_IWUSR|stat.S_IREAD)
-                audioFile = eyed3.load(file)
-                artistInfo[audioFile.tag.album] = audioFile.tag.artist
-            except:
-                #ignore garbage files and move on
-                pass
+            #folderPath is a tuple
+            #folderPath[0] is our directory
+            #folderPath[1] is blank
+            #folderPath[2] is a list of track names
+            for name in folderPath[2]:
+                file = os.path.join(folderPath[0], name)
+                try:
+                    #ensure we have read/write ability on the files
+                    os.chmod(file, stat.S_IWUSR|stat.S_IREAD)
 
-            ext = os.path.splitext(file)[1]
-            invalidExt = ['.jpg', '.png', '.url', '.tar', '.zip', '.7z']
-            if ext in invalidExt:
-                os.remove(file)
-
+                    #get artist meta data for folder naming
+                    audioFile = eyed3.load(file)
+                    artistInfo[audioFile.tag.album] = audioFile.tag.artist
+                except:
+                    #ignore garbage files, delete, and move on
+                    ext = os.path.splitext(file)[1]
+                    invalidExt = ['.jpg', '.png', '.url', '.tar', '.zip', '.7z']
+                    if ext in invalidExt:
+                        os.remove(file)
+                    pass
+        #calls filebot cli to properly set metadata on tracks
         os.system('cmd /c "filebot -rename *.mp3 --db ID3 -non-strict"') 
 
     #change directory so we have access to rename/delete
@@ -53,13 +67,16 @@ def formatAndUpload():
     index = 0
     for file in fileList:
         filePath = directory + '\\' + file
+        #make sure we remove unsafe characters for file/folder names
         renamedPath = directory + '\\' +  re.sub('[\\/:"*?<>|]+', ' ', tuple(artistInfo.items())[index][-1])
         albumPath = os.path.join(renamedPath,tuple(artistInfo.items())[index][0])
 
+        #create folders for artist -> album directories pattern
         if(not os.path.isdir(albumPath)):
             os.rename(filePath, renamedPath)
             os.mkdir(albumPath)
 
+        #get list of renamed tracks and move them to the newly created album folder
         songs = glob.glob(renamedPath +'\\' + '*.mp3')
         for song in songs:
             shutil.move(song, albumPath)
